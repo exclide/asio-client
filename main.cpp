91dcddb6833dffdb0aed189a182e1dce2bd399cd
exclide <exclide1@gmail.com>
2023-10-22 13:34:38 +0300

    base logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <boost/asio.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using boost::asio::io_context;
+using boost::asio::ip::tcp;
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Correct usage: client <ip> <port>\n";
+        return EXIT_FAILURE;
+    }
+
+    io_context ioc;
+    tcp::socket s(ioc);
+    tcp::resolver  resolver(ioc);
+    boost::asio::connect(s, resolver.resolve(argv[1], argv[2]));
+
+    size_t readSize = 128;
+
+    while (true) {
+        std::string input;
+        std::getline(std::cin, input);
+        input.push_back('\n');
+
+        boost::asio::write(s, boost::asio::buffer(input));
+
+        std::string output;
+        output.resize(readSize);
+
+        s.read_some(boost::asio::buffer(output));
+
+        std::cout << output;
+    }
+
+    return EXIT_SUCCESS;
 }
